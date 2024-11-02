94734770a916859f2a14427d3995b2c6e68f9e33
Marcelle Tabosa <marcelle.tabosa.97@gmail.com>
2024-11-02 08:14:02 -0300

    feat: creating fundamental entities

---
+++ src/LeadManagement.Domain/Entities/Job.cs
@@ -0,0 +1,13 @@
+﻿namespace LeadManagement.Domain.Entities;
+
+public class Job : Entity
+{
+    public string Description { get; set; }
+    public string Suburb { get; set; }
+    public double Price { get; set; }
+    public bool Accepted { get; set; }
+    public int JobCategoryId { get; set; }
+    public JobCategory JobCategory { get; set; }
+    public int LeadId { get; set; }
+    public Lead Lead { get; set; }
+}
