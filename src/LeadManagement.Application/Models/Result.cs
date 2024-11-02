5f0b9eafc02f707b7d80d8365ce97afece6fb53c
Marcelle Tabosa <marcelle.tabosa.97@gmail.com>
2024-11-02 16:15:51 -0300

    feat: implementing response pattern and category controller

---
+++ src/LeadManagement.Application/Models/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace LeadManagement.Application.Models;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public string Error { get; }
+    public object Value { get; }
+
+    protected Result(bool isSuccess, string error, object value)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+        Value = value;
+    }
+
+    public static Result Success(object value = null)
+    {
+        return new Result(true, null, value);
+    }
+
+    public static Result Failure(string error)
+    {
+        return new Result(false, error, null);
+    }
+}
+
